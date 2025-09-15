90a19ed6fe3967bc7cfba27bc19e29759b19a968
Greg Schmitt <gschmitt99@gmail.com>
2025-09-15 17:25:42 -0500

    location id

--- my-app/src/components/SquarePaymentForm.js
+++ my-app/src/components/SquarePaymentForm.js
@@ -6,7 +6,6 @@ import { createCatalogDataSource } from "../services/DataSourceFactory";
 // const applicationId = "sq0idp-hS_mnV5vahBjmL9cwR-gLQ";
 // this is the sandbox side
 // const applicationId = "sandbox-sq0idb-34agaTq2iqI355F57dbxKw";
-const locationId = "LE6EZZN9R77KP";
 
 const SquarePaymentForm = ({ onNonce }) => {
   const dataSource = createCatalogDataSource(process.env.REACT_APP_ENV);
@@ -19,7 +18,7 @@ const SquarePaymentForm = ({ onNonce }) => {
     let card;
     let destroyed = false;
     let applicationId;
-    let locationId;
+    const locationId = "LE6EZZN9R77KP";
 
     async function initializeSquare() {
       if (!window.Square) return;
